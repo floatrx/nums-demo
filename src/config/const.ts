64fe8a9d71664b679a3b5ee2063ec83315bc8430
Andrew floatrx <ifloatrx@gmail.com>
2024-05-22 17:53:05 +0300

    update const (level easy)

--- src/config/const.ts
+++ src/config/const.ts
@@ -9,7 +9,7 @@ export const ONE_MINUTE = 60 * ONE_SECOND;
 export const SHUFFLE_REFRESH_INTERVAL = ONE_MINUTE; // refresh after 5 minutes
 export const BOOSTER_RESET_INTERVAL = 5 * ONE_SECOND; // reset after 5 seconds
 
-export const SHUFFLE_MAX = 1;
-export const BOOSTER_MAX = 10;
+export const SHUFFLE_MAX = 10;
+export const BOOSTER_MAX = 100;
 export const SCORE_DEFAULT = 2;
 export const SCORE_TEN = 10;
