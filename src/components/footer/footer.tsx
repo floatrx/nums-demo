aa10dc7f5b711b638245b0c6fd1c1c06496251c0
Andrew floatrx <ifloatrx@gmail.com>
2024-05-23 02:16:13 +0300

    add footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -3,6 +3,11 @@ import s from './footer.module.css';
 
 interface IProps {}
 
-export const Footer: FC<IProps> = () => {
-  return <footer className={s.footer}></footer>;
-};
+export const Footer: FC<IProps> = () => (
+  <footer className={s.footer}>
+    Nums • 2024, floatrx. Check out the source code and game rules on{' '}
+    <a href="https://github.com/floatrx/nums-demo" target="_blank" rel="noopener noreferrer">
+      GitHub
+    </a>
+  </footer>
+);
