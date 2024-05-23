cad4d0e003ac08b5200dfce6075290fc10d599f2
Andrew floatrx <ifloatrx@gmail.com>
2024-05-23 21:04:38 +0300

    fix tsconfig.json

--- src/types/game.ts
+++ src/types/game.ts
@@ -1,17 +1,17 @@
-export type TCoordinates = [number, number];
-
-export type TBoardSize = {
-  rows: number;
-  cols: number;
-};
+export type TCoordinates = [number, number]; // [row, col]
 
 export type TCell = {
   value: number;
   coordinates: TCoordinates;
 };
 
-export type TBoard = TCell[][];
+export type TCellsQueue = TCoordinates[]; // Queue of cells (selected & solved)
 
-export type TCellsQueue = TCoordinates[];
+export type TBoardSize = {
+  rows: number;
+  cols: number;
+};
+
+export type TBoard = TCell[][];
 
 export type TTimeout = ReturnType<typeof setTimeout> | null;
