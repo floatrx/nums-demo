8f96e725764986bcdd2c2bb11313d9d1ba33e298
Andrew floatrx <ifloatrx@gmail.com>
2024-05-24 14:49:01 +0300

    update pwa config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig, loadEnv } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
-import fs from 'fs';
 import path from 'node:path';
-
-// Load existing manifest.json
-const manifestPath = path.resolve(__dirname, 'public/manifest.json');
-const manifest = JSON.parse(fs.readFileSync(manifestPath, 'utf-8'));
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -16,8 +11,60 @@ export default defineConfig(({ mode }) => {
       react(),
       VitePWA({
         registerType: 'autoUpdate',
-        includeAssets: ['favicon.svg'], // you can include more assets like 'robots.txt', 'apple-touch-icon.png', etc.
-        manifest, // use the loaded manifest
+        includeAssets: ['favicon.svg', 'icon/favicon.png', 'icon/size-48.png', 'icon/size-192.png', 'icon/size-512.png'],
+        manifest: {
+          short_name: 'Nums',
+          name: 'Nums Game',
+          icons: [
+            {
+              src: 'icon/favicon.png',
+              sizes: '16x16',
+              type: 'image/png',
+            },
+            {
+              src: 'icon/favicon.png',
+              sizes: '32x32',
+              type: 'image/png',
+            },
+            {
+              src: 'icon/size-48.png',
+              sizes: '48x48',
+              type: 'image/png',
+            },
+            {
+              src: 'icon/size-192.png',
+              sizes: '192x192',
+              type: 'image/png',
+            },
+            {
+              src: 'icon/size-512.png',
+              sizes: '512x512',
+              type: 'image/png',
+            },
+          ],
+          start_url: '.',
+          display: 'standalone',
+          theme_color: '#06040C',
+          background_color: '#06040C',
+        },
+        workbox: {
+          // Precaching
+          globPatterns: ['**/*.{js,css,html,svg,mp3}'],
+          // Runtime Caching
+          runtimeCaching: [
+            {
+              urlPattern: /^https:\/\/api\.myapp\.com\/.*/,
+              handler: 'NetworkFirst',
+              options: {
+                cacheName: 'api-cache',
+                expiration: {
+                  maxEntries: 10,
+                  maxAgeSeconds: 60 * 60 * 24, // 1 day
+                },
+              },
+            },
+          ],
+        },
       }),
     ],
     server: {
